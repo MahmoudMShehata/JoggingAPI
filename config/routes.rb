0705f16c038ec910c66afe86b8804c0ad775e1c0
MahmoudMShehata <MahmoudCIT@gmail.com>
2022-07-05 01:00:42 +0200

    postman collection & missing routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   post '/signup',      to: 'users#create'
   get '/login',        to: 'users#login'
+  get '/weeklyreport', to: 'jogs#weekly_report'
+  get '/filterjogs',   to: 'jogs#filter_from_to'
 end
